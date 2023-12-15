4d0a04da256f1ddba45176eb48c36d1e4aba6a51
navyney <boontharika_kor@cmu.ac.th>
2023-12-15 16:43:58 +0700

    finished

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N,int M)
+{
+    if(N <= 0 || M <= 0)
+    {
+    cout <<"Invalid input";
+    }
+    else
+    {
+        for(int i = 1 ; i<= N ; i++)
+        {
+            for(int j = 1 ; j <= M; j++)
+            {
+            cout<<"O";
+            }
+        cout << "\n" ;
+        }
+    }
+}
 
 int main(){
 	
